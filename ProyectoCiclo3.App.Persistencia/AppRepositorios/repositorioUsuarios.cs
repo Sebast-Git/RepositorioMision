b705449a59c26c89496442b90c85b2cdb1af13e6
Sebast-Git <daniel.burbano@correounivalle.edu.co>
2021-10-06 11:46:57 -0500

    update 05-10-2021

--- ProyectoCiclo3.App.Persistencia/AppRepositorios/repositorioUsuarios.cs
+++ ProyectoCiclo3.App.Persistencia/AppRepositorios/repositorioUsuarios.cs
@@ -8,58 +8,50 @@ namespace ProyectoCiclo3.App.Persistencia.AppRepositorios
     public class RepositorioUsuario
     {
         List<Usuario> Usuario;
+
+        private readonly AppContext _appContext = new AppContext(); 
  
-    public RepositorioUsuario()
-        {
-            Usuario= new List<Usuario>()
-            {
-                new Usuario{id=1,nombre="Sebastian",apellidos= "Burbano",direccion= "Carrera 27",telefono= "12345679875",ciudad= "Cali"},
-                new Usuario{id=2,nombre="Camila",apellidos= "Perez",direccion= "Calle 96",telefono= "12345678953",ciudad= "Bogota"},
-                new Usuario{id=3,nombre="Maria",apellidos= "Goemz",direccion= "Avenida sur -14",telefono= "4567258745",ciudad= "Manizales"}
- 
-            };
-        }
  
         public IEnumerable<Usuario> GetAll()
         {
-            return Usuario;
+            return _appContext.Usuario;
         }
  
         public Usuario GetUsuarioWithId(int id){
-            return Usuario.SingleOrDefault(b => b.id == id);
+            return _appContext.Usuario.Find(id);
         }
 
         public Usuario Create(Usuario newUsuario)
         {
-           if(Usuario.Count > 0){
-           newUsuario.id=Usuario.Max(r => r.id) +1; 
-            }else{
-               newUsuario.id = 1; 
-            }
-           Usuario.Add(newUsuario);
-           return newUsuario;
+            var addUsuario = _appContext.Usuario.Add(newUsuario);
+            _appContext.SaveChanges();
+            return addUsuario.Entity;
         }
 
         public Usuario Update(Usuario newUsuario){
-
-            var user= Usuario.SingleOrDefault(b => b.id == newUsuario.id);
-
+            var user = _appContext.Usuario.Find(newUsuario.id);
             if(user != null){
                 user.nombre = newUsuario.nombre;
                 user.apellidos = newUsuario.apellidos;
                 user.direccion = newUsuario.direccion;
                 user.telefono = newUsuario.telefono;
                 user.ciudad = newUsuario.ciudad;
+                //Guardar en base de datos
+                 _appContext.SaveChanges();
             }
         return user;
         }
 
+
         public void Delete(int id)
         {
-        var user= Usuario.SingleOrDefault(b => b.id == id);
-        Usuario.Remove(user);
-        return;
+        var user = _appContext.Usuario.Find(id);
+        if (user == null)
+            return;
+        _appContext.Usuario.Remove(user);
+        _appContext.SaveChanges();
         }
+
 
     }
 
---
+++ ProyectoCiclo3.App.Persistencia/Migrations/20211006011322_Migra3.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProyectoCiclo3.App.Persistencia.Migrations
+{
+    public partial class Migra3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "telefono",
+                table: "Servicio",
+                newName: "origen");
+
+            migrationBuilder.RenameColumn(
+                name: "nombre",
+                table: "Servicio",
+                newName: "hora");
+
+            migrationBuilder.RenameColumn(
+                name: "direccion",
+                table: "Servicio",
+                newName: "fecha");
+
+            migrationBuilder.RenameColumn(
+                name: "apellidos",
+                table: "Servicio",
+                newName: "encomienda");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ciudad",
+                table: "Usuario",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "destino",
+                table: "Servicio",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "peso",
+                table: "Encomiendas",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ciudad",
+                table: "Usuario");
+
+            migrationBuilder.DropColumn(
+                name: "destino",
+                table: "Servicio");
+
+            migrationBuilder.RenameColumn(
+                name: "origen",
+                table: "Servicio",
+                newName: "telefono");
+
+            migrationBuilder.RenameColumn(
+                name: "hora",
+                table: "Servicio",
+                newName: "nombre");
+
+            migrationBuilder.RenameColumn(
+                name: "fecha",
+                table: "Servicio",
+                newName: "direccion");
+
+            migrationBuilder.RenameColumn(
+                name: "encomienda",
+                table: "Servicio",
+                newName: "apellidos");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "peso",
+                table: "Encomiendas",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}

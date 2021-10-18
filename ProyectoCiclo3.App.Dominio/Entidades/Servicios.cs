8da081253522ef62bc35485312e4ef92d4a7ae42
Sebast-Git <daniel.burbano@correounivalle.edu.co>
2021-10-17 21:04:15 -0500

    UpdateM3

--- ProyectoCiclo3.App.Dominio/Entidades/Servicios.cs
+++ ProyectoCiclo3.App.Dominio/Entidades/Servicios.cs
@@ -4,7 +4,7 @@ namespace ProyectoCiclo3.App.Dominio{
     public int id { get; set; }
     public Usuario origen { get; set; }
     public Usuario destino { get; set; }
-    public DateTime fecha { get; set; }
+    public string fecha { get; set; }
     public string hora { get; set; }
     public Encomienda encomienda { get; set; }
     //comentariopublic string codigo { get; set; }
